8ff100567399dcca388df3fd85da321f89f7f8a3
ravyrafael <ra.moreira@reply.com>
2020-11-02 19:36:37 -0300

    commit

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
-var video = document.querySelector("#player")
-var playPause = document.querySelector("#play-pause")
-var currentTime = document.querySelector("#current-time")
-var totalTime = document.querySelector("#total-time")
-var videoTime = document.querySelector("#video-time")
-var fullScreen = document.querySelector("#full-screen")
+var videoPlayer = {
+    currentVideo: 0,
+    getDom(){
+        this.video = document.querySelector("#player");
+        this.playPause = document.querySelector("#play-pause");
+        this.currentTime = document.querySelector("#current-time");
+        this.totalTime = document.querySelector("#total-time");
+        this.videoTime = document.querySelector("#video-time");
+        this.fullScreen = document.querySelector("#full-screen");
+        this.nextArrow = document.querySelector("#next-arrow");
+        this.title = document.querySelector("#title");
+        
+    },
 
+    load() {
+        this.videoObject = window.videos[this.currentVideo]
+        this.video.src = `./files/${this.videoObject.file}`
+    },
 
+    updateVideoTime(value){
+        this.video.currentTime = value
+        var minutes = Math.floor(value / 60).toString().padStart(2, 0)
+        var seconds = Math.floor(value % 60).toString().padStart(2, 0)
+        this.currentTime.innerHTML = `${minutes}:${seconds}`
+    },
+    updateBarTime(value){
+            var minutes = Math.floor(this.video.currentTime / 60).toString().padStart(2, 0)
+            var seconds = Math.floor(this.video.currentTime % 60).toString().padStart(2, 0)
+            this.currentTime.innerHTML = `${minutes}:${seconds}`
+            this.videoTime.value = this.video.currentTime
+    },
 
-video.src = `./files/${window.videos[0].file}`
+    play() {
+            if (this.playPause.innerText == "play_arrow") {
+                this.video.play()
+                this.playPause.innerHTML = '<i class="material-icons">pause</i>';
+            }
+            else {
+                this.video.pause()
+                this.playPause.innerHTML = '<i class="material-icons">play_arrow</i>';
+            }
+    },
+    handleFullScreen() {
+            this.video.requestFullscreen()
+    },
+    next(){
+        if(this.currentVideo < window.videos.length - 1)
+            this.currentVideo += 1 
+        else
+            this.currentVideo = 0
+        this.load()
+    },
+    back(){
+        if(this.currentVideo > 0)
+            this.currentVideo -= 1 
+        this.load()
 
-video.onloadeddata  = ()=>{
-    var minutes = Math.floor(video.duration/60).toString()
-    var seconds = Math.floor(video.duration%60).toString()
-    totalTime.innerHTML = `${minutes.padStart(2,0)}:${seconds.padStart(2,0)}`
-    videoTime.max = video.duration;
-    videoTime.onchange = function(event){
-        video.currentTime = event.target.value
+    },
+    startActions(){
+        this.video.ontimeupdate = () =>  this.updateBarTime(this.video.currentTime)       
+        this.video.onended = () => {
+            this.next();
+            this.play();
+        }
+        this.videoTime.onchange = () => this.updateVideoTime(this.videoTime.value)
+        this.videoTime.oninput = () => this.updateVideoTime(this.videoTime.value)
+        this.playPause.onclick = (event) => this.play()
+        this.fullScreen.onclick = (event) =>this.handleFullScreen()
+        this.nextArrow.onclick = (event) =>this.next()
+
+        this.video.onloadeddata = () =>{
+            this.title.innerText = `${this.videoObject.title} - ${this.currentVideo + 1} / ${window.videos.length} `
+            var minutes = Math.floor(this.video.duration / 60).toString()
+            var seconds = Math.floor(this.video.duration % 60).toString()
+            this.totalTime.innerHTML = `${minutes.padStart(2, 0)}:${seconds.padStart(2, 0)}`
+            this.videoTime.max = this.video.duration;
+        }
     }
-    var playInterval = setInterval(()=>{
-        var minutes = Math.floor(video.currentTime/60).toString().padStart(2,0)
-        var seconds = Math.floor(video.currentTime%60).toString().padStart(2,0)
-        currentTime.innerHTML = `${minutes}:${seconds}`
-        videoTime.value = video.currentTime
-    }, 100)
 }
+videoPlayer.getDom()
+videoPlayer.startActions();
+videoPlayer.load()
 
-
-
-playPause.onclick = (event)=>{
-    if(playPause.innerText == "play_arrow"){
-        video.play()
-        playPause.innerHTML = '<i class="material-icons">pause</i>';
-    }
-    else{
-        video.pause()
-        playPause.innerHTML = '<i class="material-icons">play_arrow</i>';
-    }
-}
-
-fullScreen.onclick = (event)=>{
-    video.requestFullscreen()
-}
